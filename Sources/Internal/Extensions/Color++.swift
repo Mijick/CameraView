a314b4f6a4cf5039dce1f944406c987585feab44
Tomasz Kurylik <fulcrumone@icloud.com>
2024-05-01 19:38:53 +0200

    1

--- Sources/Internal/Extensions/Color++.swift
+++ Sources/Internal/Extensions/Color++.swift
@@ -12,20 +12,23 @@
 import SwiftUI
 
 extension Color {
-    static var accent: Color = .init(red: 8.toRGB(), green: 127.toRGB(), blue: 140.toRGB())
-    static var yellow: Color = .init(red: 240.toRGB(), green: 200.toRGB(), blue: 8.toRGB())
-    static var red: Color = .init(red: 213.toRGB(), green: 41.toRGB(), blue: 65.toRGB())
-    static var background: Color = .init(red: 4.toRGB(), green: 4.toRGB(), blue: 4.toRGB())
+    static var accent: Color = .init(hex: 0x087F8C)
+    static var yellow: Color = .init(hex: 0xF0C808)
+    static var red: Color = .init(hex: 0xD52941)
+    static var background: Color = .init(hex: 0x040408)
 }
 extension UIColor {
-    static var accent: UIColor = .init(red: 8.toRGB(), green: 127.toRGB(), blue: 140.toRGB(), alpha: 1)
-    static var yellow: UIColor = .init(red: 240.toRGB(), green: 200.toRGB(), blue: 8.toRGB(), alpha: 1)
-    static var red: UIColor = .init(red: 213.toRGB(), green: 41.toRGB(), blue: 65.toRGB(), alpha: 1)
-    static var background: UIColor = .init(red: 4.toRGB(), green: 4.toRGB(), blue: 4.toRGB(), alpha: 1)
+    static var accent: UIColor = .init(hex: 0x087F8C)
+    static var yellow: UIColor = .init(hex: 0xF0C808)
+    static var red: UIColor = .init(hex: 0xD52941)
+    static var background: UIColor = .init(hex: 0x040408)
 }
 
 
 // MARK: - Helpers
-fileprivate extension Int {
-    func toRGB() -> CGFloat { CGFloat(self) / 255 }
+fileprivate extension Color {
+    init(hex: UInt) { self.init(.sRGB, red: Double((hex >> 16) & 0xff) / 255, green: Double((hex >> 08) & 0xff) / 255, blue: Double((hex >> 00) & 0xff) / 255) }
+}
+fileprivate extension UIColor {
+    convenience init(hex: UInt) { self.init(red: Double((hex >> 16) & 0xff) / 255, green: Double((hex >> 08) & 0xff) / 255, blue: Double((hex >> 00) & 0xff) / 255, alpha: 1) }
 }
