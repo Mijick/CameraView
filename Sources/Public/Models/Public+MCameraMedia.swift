a4267f0a15110a5d2887f3b4e806da96536d3bf6
Tomasz Kurylik <fulcrumone@icloud.com>
2024-06-13 18:44:17 +0200

    1

--- Sources/Public/Models/Public+MCameraMedia.swift
+++ Sources/Public/Models/Public+MCameraMedia.swift
@@ -9,18 +9,37 @@
 //  Copyright ©2024 Mijick. Licensed under MIT License.
 
 
-import Foundation
+import AVKit
 
 public struct MCameraMedia {
     private let data: Any?
-
-    init(data: Any?) { self.data = data }
 }
 
 // MARK: - Access to Data
 public extension MCameraMedia {
-    var image: Data? { data as? Data }
+    var image: CIImage? { data as? CIImage }
     var video: URL? { data as? URL }
+}
+
+// MARK: - Initialisers
+extension MCameraMedia {
+    static func create(imageData: AVCapturePhoto, filters: [CIFilter]) -> Self? {
+        guard let cgImage = imageData.cgImageRepresentation() else { return nil }
+
+        let ciImage = CIImage(cgImage: cgImage)
+        let ciImageWithFiltersApplied = ciImage.applyingFilters(filters)
+
+        let capturedImage = MCameraMedia(data: ciImageWithFiltersApplied)
+        return capturedImage
+    }
+    static func create(videoData: URL, filters: [CIFilter]) -> Self {
+        fatalError()
+    }
+}
+private extension MCameraMedia {
+    static func a() {
+
+    }
 }
 
 // MARK: - Equatable
