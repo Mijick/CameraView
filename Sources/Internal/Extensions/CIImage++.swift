4bded5a7241ba7d4956063368be2968a9229ca74
Tomasz Kurylik <fulcrumone@icloud.com>
2024-06-14 16:32:20 +0200

    1

--- Sources/Internal/Extensions/CIImage++.swift
+++ Sources/Internal/Extensions/CIImage++.swift
@@ -13,7 +13,7 @@ import SwiftUI
 
 extension CIImage {
     func applyingFilters(_ filters: [CIFilter]) -> CIImage {
-        var ciImage = clampedToExtent()
+        var ciImage = self
         filters.forEach {
             $0.setValue(ciImage, forKey: kCIInputImageKey)
             ciImage = $0.outputImage ?? ciImage
