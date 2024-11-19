ca24241d01bf5b59b1c847f65427c823fc6ff9e8
Tomasz Kurylik <fulcrumone@icloud.com>
2024-11-19 20:24:44 +0100

    1

--- Sources/Internal/Extensions/Bundle++.swift
+++ Sources/Internal/Extensions/Bundle++.swift
@@ -12,5 +12,10 @@
 import Foundation
 
 extension Bundle {
-    static var mijick: Bundle { .init(identifier: "MijickCameraView-MijickCameraView-resources") ?? .main }
+    static var mijick: Bundle {
+        .allBundles
+        .compactMap { $0.resourceURL?.appendingPathComponent("MijickCameraView_MijickCameraView", isDirectory: false).appendingPathExtension("bundle") }
+        .compactMap { Bundle(url: $0) }
+        .first ?? .main
+    }
 }
