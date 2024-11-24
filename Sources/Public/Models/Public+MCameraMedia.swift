0b79d42f474a9120c0a5d3482adcc0ce0664da70
Tomasz Kurylik <fulcrumone@icloud.com>
2024-11-24 13:22:13 +0100

    1

--- Sources/Public/Models/Public+MCameraMedia.swift
+++ Sources/Public/Models/Public+MCameraMedia.swift
@@ -11,19 +11,20 @@
 
 @preconcurrency import AVKit
 
-public struct MCameraMedia {
-    private let data: Any
+public struct MCameraMedia: Sendable {
+    private let image: UIImage?
+    private let video: URL?
 
-    init?(data: Any?) { switch data {
-        case .some(let data): self.data = data
-        case nil: return nil
-    }}
+    init?(data: Any?) {
+        if let image = data as? UIImage { self.image = image; self.video = nil }
+        else if let video = data as? URL { self.video = video; self.image = nil }
+        else { return nil }
+    }
 }
 
-// MARK: - Access to Data
 public extension MCameraMedia {
-    var image: UIImage? { data as? UIImage }
-    var video: URL? { data as? URL }
+    func getImage() -> UIImage? { image }
+    func getVideo() -> URL? { video }
 }
 
 // MARK: - Image Initialiser
