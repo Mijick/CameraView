388633592474b27c467eec3b23df8612fdfffc4c
Tomasz Kurylik <fulcrumone@icloud.com>
2024-11-29 00:38:25 +0100

    1

--- Tests/Tests+CameraManager.swift
+++ Tests/Tests+CameraManager.swift
@@ -19,11 +19,32 @@ import SwiftUI
 
 // MARK: Setup
 extension CameraManagerTests {
-    @Test("Setup") func setup() {
+    @Test("Setup with default attributes") func setupWithDefaultAttributes() async {
         cameraManager.setup(in: .init())
 
+        #expect(!cameraManager.isRunning)
         #expect(cameraManager.captureSession.isRunning)
         #expect(cameraManager.captureSession.outputs.count == 3)
-        #expect(cameraManager.captureSession.deviceInputs.count == 3)
+        #expect(cameraManager.captureSession.deviceInputs.count == 2)
+        #expect(cameraManager.motionManager.accelerometerUpdateInterval > 0)
+
+        await Task.sleep(seconds: 2)
+
+        #expect(cameraManager.isRunning)
+    }
+    @Test("Setup with custom attributes") func setupWithCustomAttributes() {
+    }
+
+
+    // sprawdzić czy się inicjalizuje z customowymi wartościami
+}
+
+
+
+
+
+extension Task where Success == Never, Failure == Never {
+    static func sleep(seconds: CGFloat) async {
+        try! await Task.sleep(nanoseconds: UInt64(seconds * 1_000_000_000))
     }
 }
