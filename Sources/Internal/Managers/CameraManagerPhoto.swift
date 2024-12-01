6484acb9d1004d26af0bf44114638a02edad2b6f
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-01 20:53:47 +0100

    1

--- Sources/Internal/Managers/CameraManagerPhoto.swift
+++ Sources/Internal/Managers/CameraManagerPhoto.swift
@@ -12,16 +12,56 @@
 import AVKit
 
 @MainActor class CameraManagerPhoto: NSObject {
-    var parent: CameraManager!
+    private(set) var parent: CameraManager!
+    private(set) var photoOutput: AVCapturePhotoOutput = .init()
 }
 
+// MARK: Setup
+extension CameraManagerPhoto {
+    func setup(parent: CameraManager) throws {
+        self.parent = parent
+        try self.parent.captureSession.add(output: photoOutput)
+    }
+}
+
+
+// MARK: - CAPTURE PHOTO
+
+
+
+// MARK: Trigger
+extension CameraManagerPhoto {
+    func capturePhoto() {
+        let settings = getPhotoOutputSettings()
+
+        configureOutput()
+        photoOutput.capturePhoto(with: settings, delegate: self)
+        parent.cameraMetalView.performImageCaptureAnimation()
+    }
+}
+private extension CameraManagerPhoto {
+    func getPhotoOutputSettings() -> AVCapturePhotoSettings {
+        let settings = AVCapturePhotoSettings()
+        settings.flashMode = parent.attributes.flashMode.get()
+        return settings
+    }
+    func configureOutput() {
+        guard let connection = photoOutput.connection(with: .video), connection.isVideoMirroringSupported else { return }
+
+        connection.isVideoMirrored = parent.attributes.mirrorOutput ? parent.attributes.cameraPosition != .front : parent.attributes.cameraPosition == .front
+        connection.videoOrientation = parent.attributes.deviceOrientation
+    }
+}
+
+// MARK: Receive Data
 extension CameraManagerPhoto: @preconcurrency AVCapturePhotoCaptureDelegate {
     func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: (any Swift.Error)?) {
         parent.attributes.capturedMedia = .create(imageData: photo, orientation: fixedFrameOrientation(), filters: parent.attributes.cameraFilters)
     }
 }
 private extension CameraManagerPhoto {
-    func fixedFrameOrientation() -> CGImagePropertyOrientation { guard UIDevice.current.orientation != parent.attributes.deviceOrientation.toDeviceOrientation() else { return parent.frameOrientation }
+    func fixedFrameOrientation() -> CGImagePropertyOrientation {
+        guard UIDevice.current.orientation != parent.attributes.deviceOrientation.toDeviceOrientation() else { return parent.frameOrientation }
         return switch (parent.attributes.deviceOrientation, parent.attributes.cameraPosition) {
             case (.portrait, .front): .left
             case (.portrait, .back): .right
@@ -35,7 +75,3 @@ private extension CameraManagerPhoto {
         }
     }
 }
-
-
-
-// zadaniem klasy jest otrzymanie zdjęcia z aparatu i jego obrobienie
