7f1bbe07ce3e24101485ba6ffbf2e1ae97cd11be
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-03 18:08:46 +0100

    1

---
+++ Sources/Internal/Managers/FocusIndicatorConfig.swift
@@ -0,0 +1,18 @@
+//
+//  FocusIndicatorConfig.swift of MijickCamera
+//
+//  Created by Tomasz Kurylik. Sending ❤️ from Kraków!
+//    - Mail: tomasz.kurylik@mijick.com
+//    - GitHub: https://github.com/FulcrumOne
+//    - Medium: https://medium.com/@mijick
+//
+//  Copyright ©2024 Mijick. All rights reserved.
+
+
+import SwiftUI
+
+class FocusIndicatorConfig {
+    var image: UIImage = .init(resource: .iconCrosshair)
+    var tintColor: UIColor = .yellow
+    var size: CGFloat = 92
+}
