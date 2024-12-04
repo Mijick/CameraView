ccbed12b0577466bdb232913623f7d60cdfe51d8
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-04 01:08:57 +0100

    1

--- Sources/Internal/Managers/CameraManagerAttributes.swift
+++ Sources/Internal/Managers/CameraManagerAttributes.swift
@@ -18,14 +18,14 @@ struct CameraManagerAttributes {
     var outputType: CameraOutputType = .photo
     var cameraPosition: CameraPosition = .back
     var isAudioSourceAvailable: Bool = true
-    var cameraFilters: [CIFilter] = []
     var zoomFactor: CGFloat = 1.0
     var flashMode: CameraFlashMode = .off
     var torchMode: CameraTorchMode = .off
-    var cameraExposure: CameraExposure = .init()
-    var hdrMode: CameraHDRMode = .auto
     var resolution: AVCaptureSession.Preset = .hd1920x1080
     var frameRate: Int32 = 30
+    var cameraExposure: CameraExposure = .init()
+    var hdrMode: CameraHDRMode = .auto
+    var cameraFilters: [CIFilter] = []
     var mirrorOutput: Bool = false
     var isGridVisible: Bool = true
 
