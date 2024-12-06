6593094dea757144c12ea37a2b3e966b1a7774a9
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-06 15:24:55 +0100

    1

--- Sources/Internal/To be cleaned (1)/DefaultCameraScreen.swift
+++ Sources/Internal/To be cleaned (1)/DefaultCameraScreen.swift
@@ -53,15 +53,7 @@ private extension DefaultCameraScreen {
         }
     }
     func createBottomView() -> some View {
-        ZStack {
-            createLightButton()
-            createCaptureButton()
-            createChangeCameraButton()
-        }
-        .frame(maxWidth: .infinity)
-        .padding(.top, 16)
-        .padding(.bottom, 12)
-        .padding(.horizontal, 32)
+        DefaultCameraScreen.BottomBar(parent: self)
     }
 }
 private extension DefaultCameraScreen {
@@ -186,3 +178,110 @@ extension DefaultCameraScreen { struct Config {
     var flipButtonVisible: Bool = true
     var flashButtonVisible: Bool = true
 }}
+
+
+
+
+
+
+
+
+extension DefaultCameraScreen { struct BottomBar: View {
+    let parent: DefaultCameraScreen
+
+
+    var body: some View {
+        ZStack {
+            createLightButton()
+            createCaptureButton()
+            createChangeCameraButton()
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.top, 16)
+        .padding(.bottom, 12)
+        .padding(.horizontal, 32)
+    }
+}}
+private extension DefaultCameraScreen.BottomBar {
+    @ViewBuilder func createLightButton() -> some View { if isLightButtonActive {
+        DefaultCameraScreen.BottomButton(
+            icon: .mijickIconLight,
+            iconRotationAngle: parent.iconAngle,
+            active: parent.lightMode == .on,
+            action: parent.changeLightMode
+        )
+        .matchedGeometryEffect(id: "button-bottom-left", in: parent.namespace)
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }}
+    @ViewBuilder func createCaptureButton() -> some View { if isCaptureButtonActive {
+        DefaultCameraScreen.CaptureButton(
+            outputType: parent.cameraOutputType,
+            isRecording: parent.isRecording,
+            action: parent.captureOutput
+        )
+    }}
+    @ViewBuilder func createChangeCameraButton() -> some View { if isChangeCameraButtonActive {
+        DefaultCameraScreen.BottomButton(
+            icon: .mijickIconChangeCamera,
+            iconRotationAngle: parent.iconAngle,
+            active: false,
+            action: changeCameraPosition
+        )
+        .matchedGeometryEffect(id: "button-bottom-right", in: parent.namespace)
+        .frame(maxWidth: .infinity, alignment: .trailing)
+    }}
+}
+
+private extension DefaultCameraScreen.BottomBar {
+    func changeLightMode() {
+        do { try parent.setLightMode(parent.lightMode.next()) }
+        catch {}
+    }
+    func changeCameraPosition() { Task {
+        do { try await parent.setCameraPosition(parent.cameraPosition.next()) }
+        catch {}
+    }}
+}
+private extension DefaultCameraScreen.BottomBar {
+    var isLightButtonActive: Bool { parent.config.lightButtonVisible && parent.hasLight }
+    var isCaptureButtonActive: Bool { parent.config.captureButtonVisible }
+    var isChangeCameraButtonActive: Bool { parent.config.changeCameraButtonVisible && !parent.isRecording }
+}
+
+
+
+
+
+
+
+
+
+extension DefaultCameraScreen { struct TopBar: View {
+
+
+    var body: some View {
+        EmptyView()
+    }
+}}
+private extension DefaultCameraScreen.TopBar {
+
+}
+private extension DefaultCameraScreen.TopBar {
+
+}
+private extension DefaultCameraScreen.TopBar {
+
+}
+private extension DefaultCameraScreen.TopBar {
+
+}
+private extension DefaultCameraScreen.TopBar {
+
+}
+private extension DefaultCameraScreen.TopBar {
+
+}
+private extension DefaultCameraScreen.TopBar {
+
+}
+
