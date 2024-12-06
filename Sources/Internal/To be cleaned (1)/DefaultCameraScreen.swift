e9d01a5db6544f0ba3ed3c6f8393bb0ab607d4cd
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-06 14:55:15 +0100

    1

--- Sources/Internal/To be cleaned (1)/DefaultCameraScreen.swift
+++ Sources/Internal/To be cleaned (1)/DefaultCameraScreen.swift
@@ -205,25 +205,4 @@ fileprivate struct CloseButton: View {
 }
 
 // MARK: - TopButton
-extension DefaultCameraScreen { struct TopButton: View {
-    let icon: ImageResource
-    let iconRotationAngle: Angle
-    let action: () -> ()
 
-
-    var body: some View {
-        Button(action: action, label: createButtonLabel)
-    }
-}}
-private extension DefaultCameraScreen.TopButton {
-    func createButtonLabel() -> some View {
-        Image(icon)
-            .resizable()
-            .frame(width: 16, height: 16)
-            .foregroundColor(Color(.mijickBackgroundInverted))
-            .rotationEffect(iconRotationAngle)
-            .frame(width: 32, height: 32)
-            .background(Color(.mijickBackgroundSecondary))
-            .mask(Circle())
-    }
-}
