ea47eb660e72ad88d1c3d13d86a9b58a6cb6ce01
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-09 15:00:23 +0100

    1

--- Sources/Internal/To be cleaned (1)/Public+MCamera.swift
+++ Sources/Internal/To be cleaned (1)/Public+MCamera.swift
@@ -1,5 +1,5 @@
 //
-//  Public+MCameraController.swift of MijickCamera
+//  Public+MCamera.swift of MijickCamera
 //
 //  Created by Tomasz Kurylik. Sending ❤️ from Kraków!
 //    - Mail: tomasz.kurylik@mijick.com
