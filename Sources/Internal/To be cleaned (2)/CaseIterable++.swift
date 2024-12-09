27149a588c570ab5ea07828cb91ee8c0a1acbcff
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-10 00:45:09 +0100

    1

--- Sources/Internal/To be cleaned (2)/CaseIterable++.swift
+++ Sources/Internal/To be cleaned (2)/CaseIterable++.swift
@@ -10,9 +10,10 @@
 
 
 extension CaseIterable where Self: Equatable {
-    func next() -> Self { let allCases = Self.allCases
-        let index = allCases.firstIndex(of: self)!
-        let nextIndex = allCases.index(after: index)
-        return allCases[nextIndex == allCases.endIndex ? allCases.startIndex : nextIndex]
+    func next() -> Self {
+        guard let index = Self.allCases.firstIndex(of: self) else { return self }
+        
+        let nextIndex = Self.allCases.index(after: index)
+        return Self.allCases[nextIndex == Self.allCases.endIndex ? Self.allCases.startIndex : nextIndex]
     }
 }
