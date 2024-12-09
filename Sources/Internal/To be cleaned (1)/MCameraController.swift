74a47a8a08992016f602d903113cd50cb650282b
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-09 14:16:46 +0100

    1

--- Sources/Internal/To be cleaned (1)/MCameraController.swift
+++ Sources/Internal/To be cleaned (1)/MCameraController.swift
@@ -21,7 +21,7 @@ public struct MCameraController: View {
         ZStack(content: createContent)
             .onAppear(perform: onAppear)
             .onDisappear(perform: onDisappear)
-            .onChange(of: cameraManager.attributes.capturedMedia, perform: notifyUserOfMediaCaptured)
+            .onChange(of: cameraManager.attributes.capturedMedia, perform: onCapturedMediaChange)
     }}
 }
 private extension MCameraController {
@@ -61,9 +61,9 @@ private extension MCameraController {
         unlockScreenOrientation()
         cameraManager.cancel()
     }
-    func notifyUserOfMediaCaptured(_ capturedMedia: MCameraMedia?) {
-        if let image = capturedMedia?.getImage() { config.imageCapturedAction(image, .init(cameraController: self)) }
-        else if let video = capturedMedia?.getVideo() { config.videoCapturedAction(video, .init(cameraController: self)) }
+    func onCapturedMediaChange(_ capturedMedia: MCameraMedia?) {
+        guard capturedMedia != nil, config.capturedMediaScreen == nil else { return }
+        notifyUserOfMediaCaptured(capturedMedia)
     }
 }
 private extension MCameraController {
@@ -73,6 +73,10 @@ private extension MCameraController {
     }
     func unlockScreenOrientation() {
         config.appDelegate?.orientationLock = .all
+    }
+    func notifyUserOfMediaCaptured(_ capturedMedia: MCameraMedia?) {
+        if let image = capturedMedia?.getImage() { config.imageCapturedAction(image, .init(cameraController: self)) }
+        else if let video = capturedMedia?.getVideo() { config.videoCapturedAction(video, .init(cameraController: self)) }
     }
 }
 
