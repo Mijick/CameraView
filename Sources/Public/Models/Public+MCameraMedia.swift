88db0f2a9703daf7d3ff7cda944062e1a7216c9a
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-10 23:48:30 +0100

    1

--- Sources/Public/Models/Public+MCameraMedia.swift
+++ Sources/Public/Models/Public+MCameraMedia.swift
@@ -11,29 +11,8 @@
 
 import SwiftUI
 
-public struct MCameraMedia: Sendable {
-    private let image: UIImage?
-    private let video: URL?
-
-    init?(data: Any?) {
-        if let image = data as? UIImage { self.image = image; self.video = nil }
-        else if let video = data as? URL { self.video = video; self.image = nil }
-        else { return nil }
-    }
-}
-
-
-// MARK: Equatable
-extension MCameraMedia: Equatable {
-    public static func == (lhs: MCameraMedia, rhs: MCameraMedia) -> Bool { lhs.image == rhs.image && lhs.video == rhs.video }
-}
-
-
-
 // MARK: Getters
 public extension MCameraMedia {
     func getImage() -> UIImage? { image }
     func getVideo() -> URL? { video }
 }
-
-
