00d7da63727c9e508d233a03c31d0ea71aa037e9
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-11 18:59:21 +0100

    1

--- Sources/Public/UI/Public+UI+MCapturedMediaScreen.swift
+++ Sources/Public/UI/Public+UI+MCapturedMediaScreen.swift
@@ -19,9 +19,48 @@ import SwiftUI
 
  ## Usage
  ```swift
- 
+ struct CustomCapturedMediaScreen: MCapturedMediaScreen {
+    let capturedMedia: MCameraMedia
+    let namespace: Namespace.ID
+    let retakeAction: () -> ()
+    let acceptMediaAction: () -> ()
 
 
+    var body: some View {
+        VStack(spacing: 0) {
+            Spacer()
+            createContentView()
+            Spacer()
+            createButtons()
+        }
+    }
+ }
+ private extension CustomCapturedMediaScreen {
+    func createContentView() -> some View { ZStack {
+        if let image = capturedMedia.getImage() { createImageView(image) }
+        else { EmptyView() }
+    }}
+    func createButtons() -> some View {
+        HStack(spacing: 24) {
+            createRetakeButton()
+            createSaveButton()
+        }
+    }
+ }
+ private extension CustomCapturedMediaScreen {
+    func createImageView(_ image: UIImage) -> some View {
+        Image(uiImage: image)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .ignoresSafeArea()
+    }
+    func createRetakeButton() -> some View {
+        Button(action: retakeAction) { Text("Retake") }
+    }
+    func createSaveButton() -> some View {
+        Button(action: acceptMediaAction) { Text("Save") }
+    }
+ }
  ```
  */
 public protocol MCapturedMediaScreen: View {
