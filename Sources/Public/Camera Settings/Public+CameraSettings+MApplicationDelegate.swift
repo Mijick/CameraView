00266cd22986f993f082ff6a13eb3f0a3e7b4078
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-11 13:15:06 +0100

    1

--- Sources/Public/Camera Settings/Public+CameraSettings+MApplicationDelegate.swift
+++ Sources/Public/Camera Settings/Public+CameraSettings+MApplicationDelegate.swift
@@ -11,6 +11,28 @@
 
 import SwiftUI
 
+/**
+ Can be used to prevent the screen orientation from changing when the **MCamera** is visible on the screen.
+ See ``MCamera/lockOrientation(_:)`` for more details.
+
+ ## Usage
+ ```swift
+ @main struct App_Main: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        WindowGroup(content: ContentView.init)
+    }
+ }
+
+
+ class AppDelegate: NSObject, MApplicationDelegate {
+    static var orientationLock = UIInterfaceOrientationMask.all
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask { AppDelegate.orientationLock }
+ }
+ ```
+ */
 public protocol MApplicationDelegate: UIApplicationDelegate {
     static var orientationLock: UIInterfaceOrientationMask { get set }
 
