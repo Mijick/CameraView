34ffa691e5a12a8a957a235884f14a8160e199da
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-11 18:37:09 +0100

    1

--- Sources/Public/Models/Public+Model+MCameraMedia.swift
+++ Sources/Public/Models/Public+Model+MCameraMedia.swift
@@ -13,6 +13,13 @@ import SwiftUI
 
 // MARK: Getters
 public extension MCameraMedia {
+    /**
+     Gets the image from the media object.
+     */
     func getImage() -> UIImage? { image }
+
+    /**
+     Gets the video URL from the media object.
+     */
     func getVideo() -> URL? { video }
 }
