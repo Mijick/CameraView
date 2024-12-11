c70229a415a9fa752b1c333b384d394c86873604
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-11 18:28:45 +0100

    1

--- Sources/Public/Camera Settings/Public+CameraSettings+MCameraController.swift
+++ Sources/Public/Camera Settings/Public+CameraSettings+MCameraController.swift
@@ -13,6 +13,15 @@ import Foundation
 
 // MARK: Available Actions
 public extension MCamera.Controller {
+    /**
+     Closes the MCamera.
+
+     See ``MCamera/setCloseMCameraAction(_:)`` for more details.
+     */
     func closeMCamera() { mCamera.config.closeMCameraAction() }
+
+    /**
+     Opens the Camera Screen.
+     */
     func reopenCameraScreen() { mCamera.manager.setCapturedMedia(nil) }
 }
