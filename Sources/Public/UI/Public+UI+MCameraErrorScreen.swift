d03be5a00ca66ee1f1396650dabfecd99de490c2
Tomasz Kurylik <fulcrumone@icloud.com>
2024-12-11 18:55:02 +0100

    1

--- Sources/Public/UI/Public+UI+MCameraErrorScreen.swift
+++ Sources/Public/UI/Public+UI+MCameraErrorScreen.swift
@@ -11,6 +11,36 @@
 
 import SwiftUI
 
+/**
+ Screen that displays an error message if one or more camera permissions are denied by the user.
+
+ - important: A view conforming to MCameraErrorScreen has to be passed directly to ``MCamera``. See ``MCamera/setErrorScreen(_:)`` for more details.
+
+
+ ## Usage
+ ```swift
+ struct ContentView: View {
+    var body: some View {
+        MCamera()
+            .setErrorScreen(CustomCameraErrorScreen.init)
+
+            // MUST BE CALLED!
+            .startSession()
+    }
+ }
+
+ // MARK: Custom Camera Error Screen
+ struct CustomCameraErrorScreen: MCameraErrorScreen {
+    let error: MCameraError
+    let closeMCameraAction: () -> ()
+
+
+    var body: some View {
+        Button(action: openAppSettings) { Text("Open Settings") }
+    }
+ }
+ ```
+ */
 public protocol MCameraErrorScreen: View {
     var error: MCameraError { get }
     var closeMCameraAction: () -> () { get }
